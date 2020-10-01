301a240884e91eb94a310aa5da69007120912ce2
15Andrew43 <avborovets@gmail.com>
2020-10-01 20:04:30 +0300

    contest1

--- semestr3/contest1/MergeNPieces/main.cpp
+++ semestr3/contest1/MergeNPieces/main.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
 #include <vector>
-#include <unordered_set>
 #include <iterator>
-#include <algorithm>
+#include <set>
+#include <tuple>
 
 template <typename T, typename IteratorC, typename Cmp = std::less<T>>
 IteratorC Merge(std::vector<std::vector<T>>& vec, IteratorC c, Cmp cmp = Cmp()) {
-    std::unordered_set<std::tuple<int, typename std::vector<T>::iterator>> iters;
+    std::set<std::tuple<T, int, typename std::vector<T>::iterator>> set;
     for (int i = 0; i < vec.size(); ++i) {
-        iters.insert(std::make_tuple(i, vec[i].begin()));
+        set.insert(std::make_tuple(vec[i][0], i, vec[i].begin()));
     }
-
-    while (!iters.empty()) {
-        auto first = iters.begin();
-        for (auto it = iters.begin(); it != iters.end(); ++it) {
-            if (!cmp(std::get<1>(*it), std::get<1>(*first))) continue;
-            std::get<1>(*first) = std::get<1>(*it);
-        }
-        *c = std::get<1>(*first);
+    while (!set.empty()) {
+        *c = std::get<0>(*set.begin());
         ++c;
-        ++std::get<1>(*first);
-        if (std::get<1>(*first) == vec[std::get<0>(*first)].end()) {
-            iters.erase(first);
+        int ind = std::get<1>(*set.begin());
+        auto iter = std::get<2>(*set.begin());
+        set.erase(set.begin());
+        if (vec[ind].end() != ++iter) {
+            set.insert(std::make_tuple(*iter, ind, iter));
         }
     }
     return c;
@@ -30,15 +26,17 @@ IteratorC Merge(std::vector<std::vector<T>>& vec, IteratorC c, Cmp cmp = Cmp()) 
 int main() {
     int k;
     std::cin >> k;
-    std::vector<std::vector<int>> vec(k);
+    std::vector<std::vector<int>> vec;
     for (int i = 0; i < k; ++i) {
         int n;
         std::cin >> n;
+        std::vector<int> v;
         for (int j = 0; j < n; ++j) {
             int d;
             std::cin >> d;
-            vec[i].push_back(d);
+            v.push_back(d);
         }
+        vec.push_back(v);
     }
 
     Merge(vec, std::ostream_iterator<int>(std::cout, " "));
