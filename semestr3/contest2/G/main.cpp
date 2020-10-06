a600a0937eade75248625401e8fc7228a3ccc274
15Andrew43 <avborovets@gmail.com>
2020-10-06 10:21:13 +0300

    contest2

--- semestr3/contest2/G/main.cpp
+++ semestr3/contest2/G/main.cpp
@@ -7,70 +7,93 @@
 
 using Coordinates = std::pair<int, int>;
 
-struct MyHash {
-    size_t operator()(const Coordinates& value) const noexcept {
-        return std::hash<int>{}(value.first) ^ std::hash<int>{}(value.second);
-    }
+enum Color {
+    undiscovered = 0,
+    discovered = 1,
+    processed = 2,
 };
 
-std::vector<Coordinates > getNeighbours(int N, Coordinates v) {
-    int start_x = std::get<0>(v);
-    int start_y = std::get<1>(v);
-    std::vector<Coordinates > neighbours;
-    for (int y = -2; y <= 2; ++y) {
-        for (int x = -2; x <= 2; ++x) {
-            if (start_x + x >= 1 && start_x + x <= N && start_y + y >= 1 && start_y + y <= N && abs(x) + abs(y) == 3) {
-                neighbours.emplace_back(start_x + x, start_y + y);
-            }
+std::vector<Coordinates> getPossibleMoves(const std::vector<std::vector<int>>& map, const Coordinates& start,
+                                                std::vector<std::vector<Color>>& colors) {
+    std::vector<Coordinates> possible_moves;
+    int start_y = start.first;
+    int start_x = start.second;
+    std::vector<Coordinates> moves = {std::make_pair(start_y - 1, start_x),
+                                      std::make_pair(start_y + 1, start_x),
+                                      std::make_pair(start_y, start_x - 1),
+                                      std::make_pair(start_y, start_x + 1)};
+    for (auto move: moves) {
+        if (map[move.first][move.second] <= map[start_y][start_x] && colors[move.first][move.second] == undiscovered) {
+            possible_moves.push_back(move);
         }
     }
-    return neighbours;
+    return possible_moves;
 }
-std::vector<Coordinates> BFS(int N, Coordinates from, Coordinates to) {
-    std::queue<Coordinates> q;
-    std::unordered_map<Coordinates, int, MyHash> distance;
-    std::unordered_map<Coordinates, Coordinates, MyHash> parent;
-    distance[from] = 0;
-    q.push(from);
 
-    while (!q.empty()) {
-        Coordinates v = q.front();
-        q.pop();
-        std::vector<Coordinates> neighbours = getNeighbours(N, v);
-        for (auto u: neighbours) {
-            if (distance.find(u) == distance.end()) {
-                q.push(u);
-                distance[u] = distance[v] + 1;
-                parent[u] = v;
+int DFSVisit(const std::vector<std::vector<int>>& map,
+            const Coordinates& start, std::vector<std::vector<Color>>& colors, bool& is_deapest, int& drain_cnt) {
+    colors[start.first][start.second] = discovered;
+    std::vector<Coordinates> possible_moves = getPossibleMoves(map, start, colors);
+    if (possible_moves.size() == 0) {
+//        return map[start.first][start.second];
+        is_deapest = true;
+    }
+    for (auto u: possible_moves) {
+        if (is_deapest) {
+            if (DFSVisit(map, u, colors, is_deapest, drain_cnt) != map[start.first][start.second]) {
+                is_deapest = false;
+                ++drain_cnt;
+                std:: cout << u.first << ' ' << u.second << '\n';
+                std::cout << "pep\n";
             }
+        } else {
+            DFSVisit(map, u, colors, is_deapest, drain_cnt);
         }
     }
-
-    std::vector<Coordinates > path;
-    while (to != from) {
-        path.push_back(to);
-        to = parent[to];
-    }
-    path.push_back(from);
-
-    return {path.rbegin(), path.rend()};
+    colors[start.first][start.second] = processed;
+    return map[start.first][start.second];
 }
 
+int DFS(const std::vector<std::vector<int>>& map, const Coordinates& start) {
+    bool is_deapest = false;
+    int drain_cnt = 0;
+    std::vector<std::vector<Color>> colors(map.size(), std::vector<Color>(map[0].size(), undiscovered));
+    DFSVisit(map, start, colors, is_deapest, drain_cnt);
+    return drain_cnt;
+}
 
 int main() {
     int n, m;
     std::cin >> n >> m;
     std::vector<std::vector<int>> map;
+    Coordinates max_hight_coord;
+    int max_hight = 0;
+
+    map.push_back(std::vector<int>(m + 2, 10001));
     for (int i = 0; i < n; ++i) {
         std::vector<int> line;
+        line.push_back(10001);
         for (int j = 0; j < m; ++j) {
             int hight;
             std::cin >> hight;
+            if (hight > max_hight) {
+                max_hight = hight;
+                max_hight_coord = std::make_pair(i + 1, j + 1);
+            }
             line.push_back(hight);
         }
+        line.push_back(10001);
         map.push_back(line);
     }
+    map.push_back(std::vector<int>(m + 2, 10001));
 
-    std::cout << BFS(map);
+//    for (auto line: map) {
+//        for (auto high: line) {
+//            std::cout << high << ' ';
+//        }
+//        std::cout << '\n';
+//    }
+//    std::cout << max_hight_coord.first << ' ' << max_hight_coord.second << '\n';
+    std::cout << DFS(map, max_hight_coord);
     return 0;
 }
