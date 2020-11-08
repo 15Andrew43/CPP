a40d9566e92486b5463fe6fd2c7815268aa5400d
15Andrew43 <avborovets@gmail.com>
2020-11-08 21:59:37 +0300

    contest3

---
+++ semestr3/contest3/heapG/cmake-build-debug/CMakeFiles/heapG.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/heapG.dir/main.cpp.o"
+  "heapG"
+  "heapG.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/heapG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
